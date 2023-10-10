0d88f027dd37cd95d22a8fdaac817d32e9f4fd80
Hoarfroster <hoarfroster@outlook.com>
2023-10-10 08:43:51 +0000

    update documentation for L5/5.cpp

--- Computer Thinking/L5/5.cpp
+++ Computer Thinking/L5/5.cpp
@@ -5,7 +5,13 @@ static bool isInRecursion = false;
 
 /// Requirement: use recursion to calculate sum of 0 to 99.
 ///
-/// Note that the recursion function is not following the C++ standard strictly (overriding [argc] and [argv]). It is not a good practice.
+/// Note that the recursion function overrided [argc] and [argv].
+/// Plus according to C++ standard, one should not call the main function 
+/// within the program. Thus, the program is not promised to be runnable
+/// in all platforms.
+///
+/// Though, in C, given that no relevant restrction is imposed, the program 
+/// shall be a validated one.
 
 int main(int argc, char *argv[]) {
     /// Code hereby marks the body of the recursion.
